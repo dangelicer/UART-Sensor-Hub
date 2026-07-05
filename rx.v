0917f02e07ab0f23a4743bc6e4e51739afba7003
Daniel gagnon <daniel.r.gagnon@outlook.com>
2026-07-05 12:34:31 -0500

    Fixed all rx module and testbench bugs, all tests have passed as of 12:34PM 7/5/2026

--- rx.v
+++ rx.v
@@ -11,8 +11,8 @@ module rx (
 );
 
 	localparam			BIT_COUNT_MAX	= 10'd8;
-	localparam			TICK_MAX		= 10'd16;
-	localparam			TICK_MIDDLE		= 10'd8;
+	localparam			TICK_MAX		= 10'd15;
+	localparam			TICK_MIDDLE		= 10'd7;
 
 	localparam			IDLE			= 2'b00;
 	localparam			START			= 2'b01;
@@ -20,16 +20,17 @@ module rx (
 	localparam			STOP			= 2'b11;
 
 	reg			[1:0]	state, next_state;
-	reg			[8:0]	data_byte;
-	reg			parity_err;
-	reg			framing_err;
-	reg			overrun_err;
+	reg			[7:0]	data_byte;
+	reg			[9:0]	bit_count;
+	reg					parity_err;
+	reg			[9:0]	ticks_elapsed;
+
 
 	// current state
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			state <= IDLE;
-		end else begin
+		end else if (tick) begin
 			state <= next_state;
 		end
 	end
@@ -45,19 +46,19 @@ module rx (
 				end
 			end
 			START: begin
-				if (tick == TICK_MIDDLE && ~RX) begin
+				if (ticks_elapsed == TICK_MIDDLE && ~RX) begin
 					next_state = SAMPLING;
 				end else if (RX) begin
 					next_state = IDLE;
 				end
 			end
 			SAMPLING: begin
-				if (tick == TICK_MAX && bit_count == BIT_COUNT_MAX) begin
+				if (ticks_elapsed == TICK_MAX && bit_count == BIT_COUNT_MAX) begin
 					next_state = STOP;
 				end
 			end
 			STOP: begin
-				if (tick == TICK_MAX) begin
+				if (ticks_elapsed == TICK_MAX) begin
 					next_state = IDLE;
 				end
 			end
@@ -65,36 +66,47 @@ module rx (
 	end
 
 	// datapath assignments
-	always @(*) begin
-		framing_err		= 0;
-		parity_err		= 0;
-		overrun_err		= 0;
-		wr_en			= 0;
-		data_out		= 13'b0;
-		tick = 0;
-		bit_count = 0;
+	always @(posedge clk) begin
+		if (tick) begin
+			parity_err		<= 0;
+			wr_en			<= 0;
+			data_out		<= 13'b0;
+			data_byte       <= 8'b0;
+			ticks_elapsed <= 0;
+			bit_count <= 0;
 
-		case(state)
-			SAMPLING: begin
-				if (bit_count < BIT_COUNT_MAX) begin
-					RX >> data_out;
-					tick = 0;
-					bit_count = bit_count + 1;
-				end else begin
-					parity_err = ^data_byte ^ RX;	// check parity (even parity assumed here, add ~ at beginning for odd parity)
-					tick = 0;
-					bit_count = 0;
+			case(state)
+				START: begin
+					if (ticks_elapsed < TICK_MIDDLE) begin
+						ticks_elapsed <= ticks_elapsed + 1;
+					end
 				end
-			end
-			STOP: begin
-				if (~RX) begin
-					framing_err = 1'b1;
-				end else if (queue_full) begin
-					overrun_err = 1'b1;
-				end
+				SAMPLING: begin
+					ticks_elapsed <= ticks_elapsed + 1;
+					data_byte <= data_byte; // Persist data across ticks until the next bit is sampled
+					bit_count <= bit_count;
 
-				wr_en = 1'b1;
-				data_out = {id, data_byte, parity_err, framing_err, overrun_err};
-			end
-		endcase
+					if (ticks_elapsed == TICK_MAX) begin
+						if (bit_count < BIT_COUNT_MAX) begin
+							data_byte <= {RX, data_byte[7:1]};
+							ticks_elapsed <= 0;
+							bit_count <= bit_count + 1;
+						end else begin
+							parity_err <= ^({data_byte, RX});	// check parity (even parity assumed here, add ~ at beginning for odd parity)
+							ticks_elapsed <= 0;
+						end
+					end
+				end
+				STOP: begin
+					ticks_elapsed <= ticks_elapsed + 1;
+					data_byte <= data_byte; // Keep the changed value until we store it, don't reset until we leave STOP
+					parity_err <= parity_err;
+					if (ticks_elapsed == TICK_MAX) begin
+						wr_en <= 1'b1;
+						data_out <= {id, data_byte, parity_err, ~RX, queue_full};
+					end
+				end
+			endcase
+		end
 	end
+endmodule
--- rx_tb.v
+++ rx_tb.v
@@ -14,9 +14,6 @@ module rx_tb();
 
 	reg [10:0] frame;
 	reg [12:0] expected_data_out;
-	integer parity_err;
-	integer framing_err;
-	integer overrun_err;
 	integer error_count;
 
 	baud_tick_gen baud_tick_gen_inst (
@@ -68,7 +65,7 @@ module rx_tb();
 		integer timeout;
 
 		begin
-			frame = 11'b10101001111;
+			frame = 11'b10101001110;
 
 			// test IDLE freewheele
 			for (i = 0; i < 3; i = i + 1) begin
@@ -90,7 +87,7 @@ module rx_tb();
 			end
 			@(posedge tick);
 			RX = 1;
-			repeat(3) @(posedge clk); // if we go back to IDLE tick shouldn't incriment so we use clk instead
+			repeat(3) @(posedge tick); // if we go back to IDLE tick shouldn't incriment so we use clk instead
 			if (uut.state != uut.IDLE) begin
 				$error("Test failed: state does not match expected value. Expected IDLE, got %b", uut.state);
 				error_count = error_count + 1;
@@ -98,43 +95,46 @@ module rx_tb();
 
 			// test SAMPLING
 			RX = frame[0]; // start bit
-			$display("Start bit sent, waiting for START");
-			repeat (8) @(posedge tick);
+			
+			timeout = 0;
+			while ((uut.state != uut.SAMPLING) && timeout < 250) begin
+				@(posedge tick);
+				timeout = timeout + 1;
+			end
+
+			if (timeout >= 250) begin
+				$error("Test failed: never reached SAMPLING (state=%b, ticks_elapsed=%d) timed out", uut.state, uut.ticks_elapsed);
+				error_count = error_count + 1;
+			end else if (uut.ticks_elapsed != 0) begin // tick should be 0 at start of every state
+				$error("Test failed: ticks_elapsed did not reset on state transition. Expected 0, got %d", uut.ticks_elapsed);
+				error_count = error_count + 1;
+			end
+
 			RX = frame[1]; // first data bit
-			@(posedge clk);
-			$display("First data bit sent, waiting for SAMPLING");
-			if (uut.state != uut.SAMPLING) begin
-				$error("Test failed: state does not match expected value. Expected SAMPLING, got %b", uut.state);
-				error_count = error_count + 1;
-			end
-			if (uut.ticks_elapsed != 0) begin // tick should be 0 at start of every state
-				$error("Test failed: tick did not reset on state transition. Expected 0, got %b", uut.ticks_elapsed);
-				error_count = error_count + 1;
-			end
 			repeat (10) @(posedge tick);
 			if (uut.state != uut.SAMPLING) begin
 				$error("Test failed: state does not match expected value. Expected SAMPLING, got %b", uut.state);
 				error_count = error_count + 1;
 			end
 			repeat (6) @(posedge tick);
-			if (uut.data_byte[0] != frame[1]) begin
+			if (uut.data_byte[7] != frame[1]) begin
 				$error("Test failed: data_byte didn't receive expected value. Expected %b, got %b", frame[1], uut.data_byte[0]);
 				error_count = error_count + 1;
 			end
 			repeat (6) @(posedge tick);
 			if (uut.bit_count != 1) begin
-				$error("Test failed: bit_count didn't increment. Expected 1, got %b", uut.bit_count);
+				$error("Test failed: bit_count didn't increment. Expected 1, got %d", uut.bit_count);
 				error_count = error_count + 1;
 			end
 
 			timeout = 0;
-			while ((uut.state != uut.STOP || uut.ticks_elapsed != 10) && timeout < 20000) begin
-				@(posedge clk);
+			while ((uut.state != uut.STOP || uut.ticks_elapsed != 10) && timeout < 250) begin
+				@(posedge tick);
 				timeout = timeout + 1;
 			end
 
-			if (timeout >= 20000) begin
-				$error("Test failed: never reached STOP (state=%b, tick=%d) � timed out", uut.state, uut.tick);
+			if (timeout >= 250) begin
+				$error("Test failed: never reached STOP (state=%b, ticks_elapsed=%d) timed out", uut.state, uut.ticks_elapsed);
 				error_count = error_count + 1;
 			end else begin
 				@(posedge clk);
@@ -142,7 +142,7 @@ module rx_tb();
 					$error("Test failed: state does not match expected value. Expected STOP, got %b", uut.state);
 					error_count = error_count + 1;
 				end
-		end
+			end
 
 			$display("State tests concluded");
 		end
@@ -197,16 +197,18 @@ module rx_tb();
 		$display("Reset complete");
 
 		// frame testing
-		frame = 11'b10101001101; // good frame, no errors
+		frame = 11'b10101001100; // good frame, no errors
 		send_frame(frame);
 		expected_data_out = construct_data_out(id, frame, 0, 0, 0);
 		check_data_out(expected_data_out);
 		reset();
 
-		frame = 11'b01101001101; // bad frame, all flags should get set
+		frame = 11'b01101001100; // bad frame, all flags should get set
+		queue_full = 1'b1;
 		send_frame(frame);
 		expected_data_out = construct_data_out(id, frame, 1, 1, 1);
 		check_data_out(expected_data_out);
+		queue_full = 1'b0;
 		reset();
 
 
@@ -216,20 +218,22 @@ module rx_tb();
 
 
 		// Test multiple frames back to back
-		frame = 11'b10101001101; // good frame, no errors
+		frame = 11'b10101001100; // good frame, no errors
 		send_frame(frame);
 		expected_data_out = construct_data_out(id, frame, 0, 0, 0);
 		check_data_out(expected_data_out);
 
-		frame = 11'b10101001101; // good frame, no errors
+		frame = 11'b10101001100; // good frame, no errors
 		send_frame(frame);
 		expected_data_out = construct_data_out(id, frame, 0, 0, 0);
 		check_data_out(expected_data_out);
 
-		frame = 11'b01101001101; // bad frame, all flags should get set
+		frame = 11'b01101001100; // bad frame, all flags should get set
+		queue_full = 1'b1;
 		send_frame(frame);
 		expected_data_out = construct_data_out(id, frame, 1, 1, 1);
 		check_data_out(expected_data_out);
+		queue_full = 1'b0;
 
 
 		$display("All tests concluded with %d errors", error_count);
